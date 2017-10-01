18e0805d2b3eaaad8f8d584f7d4cdb6119d9f760
7kia <il7kia@yandex.ru>
2017-10-01 22:16:10 +0400

    Исправлены замечания

--- Lab2/app/src/main/java/com/example/lab2/RssModel.java
+++ Lab2/app/src/main/java/com/example/lab2/RssModel.java
@@ -5,9 +5,28 @@ public class RssModel {
     public String mLink;
     public String mPublicationDate;
 
+
+    public RssModel()
+    {
+
+    }
+
     public RssModel(String title, String link, String pubDate) {
         this.mTitle = title;
         this.mLink = link;
         this.mPublicationDate = pubDate;
+    }
+
+    public boolean isFilled()
+    {
+        return (mTitle != null) && (mLink != null) && (mPublicationDate != null);
+    }
+
+    public void clear()
+    {
+        this.mTitle = null;
+        this.mLink = null;
+        this.mPublicationDate = null;
+
     }
 }
